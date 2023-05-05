111eb26904807787f7546b7420682db695f60f81
joaoHenrique9 <joao.gh61@gmail.com>
2023-05-04 23:21:25 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/educandoweb/workshopmongo/domain/Post.java
+++ src/main/java/com/educandoweb/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.educandoweb.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.educandoweb.workshopmongo.dto.AuthorDTO;
+import com.educandoweb.workshopmongo.dto.CommentDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,6 +30,16 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 
     @Override
     public int hashCode() {
