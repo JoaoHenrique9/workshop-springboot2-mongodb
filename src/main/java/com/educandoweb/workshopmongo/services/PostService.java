05289d810bf65d84c45916d08a73af09a296a7dc
joaoHenrique9 <joao.gh61@gmail.com>
2023-05-04 23:58:12 -0300

    Consulta com vários critérios

--- src/main/java/com/educandoweb/workshopmongo/services/PostService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.educandoweb.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -22,5 +23,10 @@ public class PostService {
 
     public List<Post> findByTitle(String title) {
         return repository.searchTitle(title);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
